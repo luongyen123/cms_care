861cff60186ca7f256e3419ac2570e9da7489a46
luongyen <luongyen1508@gmail.com>
2020-01-18 11:50:30 +0700

    update request

--- src/pages/Requested.vue
+++ src/pages/Requested.vue
@@ -54,20 +54,137 @@
                 :key="index"
               >{{itemValue(item,columnIndx)}}</td>
               <td>
-                <button class="btn ti-layout-tab-window" v-b-modal.modal-1 v-on:click="detailRequest(item.id_request)"></button>
+                <button
+                  class="btn ti-layout-tab-window"
+                  v-b-modal.modal-1
+                  v-on:click="detailRequest(item['id_request'])"
+                ></button>
               </td>
             </slot>
           </tr>
         </tbody>
       </table>
-      <b-modal id="modal-1" title="BootstrapVue">
-        <p class="my-4">Hello from modal!</p>
+      <b-modal id="modal-1" title="Detail request" size="lg">
+        <div class="form-row" v-if="responseDetail.nurse">
+          <div class="form-group col-md-12" style="margin-top: -34px;">
+            <h4>Nurse Information</h4>
+          </div>
+          <div class="form-group col-md-4" v-if="responseDetail.nurse.name">
+            <label>Name</label>
+            <input type="Text" class="form-control" v-model="responseDetail.nurse.name" disabled />
+          </div>
+          <div class="form-group col-md-4" v-if="responseDetail.nurse.phone">
+            <label>Phone</label>
+            <input type="text" class="form-control" v-model="responseDetail.nurse.phone" disabled />
+          </div>
+          <div class="form-group col-md-4" v-if="responseDetail.nurse.email">
+            <label>Email</label>
+            <input type="text" class="form-control" v-model="responseDetail.nurse.email" disabled />
+          </div>
+        </div>
+        <div class="form-row" v-if="responseDetail.patient">
+          <div class="form-group col-md-12" style="margin-top: -34px;">
+            <h4>Patient Information</h4>
+          </div>
+          <div class="form-group col-md-6" v-if="responseDetail.patient.name">
+            <label>Name</label>
+            <input type="text" class="form-control" v-model="responseDetail.patient.name" disabled />
+          </div>
+          <div class="form-group col-md-6" v-if="responseDetail.patient.birthday">
+            <label>Age</label>
+            <input
+              type="text"
+              class="form-control"
+              v-bind:value="getAge(responseDetail.patient.birthday)"
+              disabled
+            />
+          </div>
+          <div class="form-group col-md-12" v-if="responseDetail.patient">
+            <label style="font-size: 19px;">Contact</label>
+          </div>
+          <div class="form-group col-md-6" v-if="responseDetail.patient.phone">
+            <label>Phone</label>
+            <input type="text" class="form-control" v-model="responseDetail.patient.phone" disabled />
+          </div>
+          <div class="form-group col-md-6" v-if="responseDetail.patient.email">
+            <label>Email</label>
+            <input type="text" class="form-control" v-model="responseDetail.patient.email" disabled />
+          </div>
+        </div>
+        <div class="form-group col-md-12">
+          <h4>Detail request</h4>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6" v-if="responseDetail.start_date">
+            <label>Start date care</label>
+            <input
+              type="text"
+              class="form-control"
+              v-bind:value="viewDate(responseDetail.start_date)"
+              disabled
+            />
+          </div>
+          <div class="form-group col-md-6" v-if="responseDetail.start_date">
+            <label>End date care</label>
+            <input
+              type="text"
+              class="form-control"
+              v-bind:value="viewDate(responseDetail.end_date)"
+              disabled
+            />
+          </div>
+          <div class="form-group col-md-6" v-if="responseDetail.start_time">
+            <label>Start time care</label>
+            <input
+              type="text"
+              class="form-control"
+              v-bind:value="itemValue(responseDetail,'start_time')"
+              disabled
+            />
+          </div>
+          <div class="form-group col-md-6" v-if="responseDetail.end_time">
+            <label>End time care</label>
+            <input
+              type="text"
+              class="form-control"
+              v-bind:value="itemValue(responseDetail,'end_time')"
+              disabled
+            />
+          </div>
+          <div class="form-group col-md-6" v-if="responseDetail.status">
+            <label>Status</label>
+            <input
+              type="text"
+              class="form-control"
+              v-bind:value="itemValue(responseDetail,'status')"
+              disabled
+            />
+          </div>
+          <div class="form-group col-md-6" v-if="responseDetail.type">
+            <label>Type</label>
+            <input
+              type="text"
+              class="form-control"
+              v-bind:value="itemValue(responseDetail,'type')"
+              disabled
+            />
+          </div>
+          <div class="form-group col-md-6">
+            <label>Rate {{responseDetail.rate +'/5'}}</label>
+            <i class="fa fa-star" aria-hidden="true" v-for="(item, index) in 5" v-bind:key="index" v-bind:class="{ active: star(item,responseDetail.rate ) }"></i>
+          </div>
+        </div>
       </b-modal>
     </div>
   </div>
 </template>
 <script>
-import { formatTimeLog } from "../utils/dateFormat";
+import {
+  formatTimeLog,
+  Age,
+  formatDate,
+  formatTime
+} from "../utils/dateFormat";
 const columns = [
   "#",
   "Nurse",
@@ -99,12 +216,10 @@ export default {
         start_date: "",
         end_date: ""
       },
-      detaiRequest: {
+      detaiRequestForm: {
         id_request: 0
       },
-      responseDetail: {
-
-      },
+      responseDetail: {},
       data: [],
       totalPage: 0,
       name_router: "user/getRequest"
@@ -156,6 +271,9 @@ export default {
           } else {
             return "Finish";
           }
+        case "start_time":
+        case "end_time":
+          return formatTime(item[column]);
         default:
           return item[column];
       }
@@ -176,19 +294,40 @@ export default {
         return index + 1 + 10 * (this.pageActive - 1);
       }
     },
-    detailRequest(id_request){
-      console.log(id_request)
-      this.detailRequest = {
+    detailRequest(id_request) {
+      this.detaiRequestForm = {
         id_request: id_request
+      };
+      this.$store
+        .dispatch("user/detailRequest", this.detaiRequestForm)
+        .then(reponse => {
+          this.responseDetail = reponse;
+          console.log(this.responseDetail);
+        });
+    },
+    getAge(birthday) {
+      return Age(birthday);
+    },
+    viewDate(date) {
+      return formatDate(date);
+    },
+    star(item, rate) {
+      if(item <= rate) {
+        return true
+      } else {
+        return false
       }
-      this.$store.dispatch('user/detailRequest', this.detailRequest).then(reponse => {
-        this.responseDetail = reponse
-        console.log(this.responseDetail)
-      });
     }
   }
 };
 </script>
 
 <style>
+.fa-star {
+  font-size: 34px;
+  margin: 8px;
+}
+.active {
+  color: #ffe600;
+}
 </style>
